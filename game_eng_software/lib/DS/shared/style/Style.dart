591161e2b2c81da7133fcd87ecbf5aa195b9dfc4
Raphael <murdok.rapha@gmail.com>
2025-08-29 18:53:28 -0300

    desenvolvimento dos arquivos responsaveis pela cor e style do projeto, sendo eles Color.dart e Style.dart

---
+++ game_eng_software/lib/DS/shared/style/Style.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:game_eng_software/DS/shared/color/Colors.dart';
+
+//text black ------------------------------------------------------
+TextStyle normalTextStyleBlack = TextStyle(
+  color: primaryColorTextBlack,
+  fontSize: 16.0,
+  fontWeight: FontWeight.normal,
+);
+
+TextStyle headingTextStyleBlack = TextStyle(
+  color: primaryColorTextBlack,
+  fontSize: 24.0,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle subheadingTextStyleBlack = TextStyle(
+  color: primaryColorTextBlack,
+  fontSize: 20.0,
+  fontWeight: FontWeight.w500,
+);
+
+//text white ------------------------------------------------------
+
+TextStyle normalTextStyleWhite = TextStyle(
+  color: primaryColorTextWhite,
+  fontSize: 16.0,
+  fontWeight: FontWeight.normal,
+);
+
+TextStyle headingTextStyleWhite = TextStyle(
+  color: primaryColorTextWhite,
+  fontSize: 24.0,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle subheadingTextStyleWhite = TextStyle(
+  color: primaryColorTextWhite,
+  fontSize: 20.0,
+  fontWeight: FontWeight.w500,
+);
+
+// button text ---------------------------------------
+
+TextStyle buttonTextStyleBlack = TextStyle(
+  color: primaryColorTextBlack,
+  fontSize: 16.0,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle buttonTextStyleWhite = TextStyle(
+  color: primaryColorTextWhite,
+  fontSize: 16.0,
+  fontWeight: FontWeight.bold,
+);
+
+// notification message ----------------------------------------------
+
+TextStyle notificationMessageStyleBlack = TextStyle(
+  color: primaryColorTextBlack,
+  fontSize: 14.0,
+  fontWeight: FontWeight.normal,
+);
+
+TextStyle notificationMessageStyleWhite = TextStyle(
+  color: primaryColorTextWhite,
+  fontSize: 14.0,
+  fontWeight: FontWeight.normal,
+);
+
+TextStyle notificationMessageStyleRed = TextStyle(
+  color: primaryColorTextRed,
+  fontSize: 14.0,
+  fontWeight: FontWeight.normal,
+);
